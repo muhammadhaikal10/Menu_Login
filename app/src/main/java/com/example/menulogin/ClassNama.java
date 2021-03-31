004170c7d6eff819cf8c7ace92fa05e3c113a900
haika <haikal04102001@gmail.com>
2021-03-31 23:43:29 +0700

    20190140009_MuhammadHaikal

---
+++ app/src/main/java/com/example/menulogin/ClassNama.java
@@ -0,0 +1,15 @@
+package com.example.menulogin;
+
+public class ClassNama {
+    private  String Nama;
+
+    public ClassNama(String nama)
+    {
+        this.Nama = nama;
+    }
+
+    public String getNama()
+    {
+        return this.Nama;
+    }
+}
